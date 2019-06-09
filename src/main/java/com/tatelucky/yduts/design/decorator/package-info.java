a470e254b1d7f857108e7623ea570c94fee20079
汤胜 <tangsheng@souche.com>
2019-06-09 23:12:24 +0800

    first commit

---
+++ src/main/java/com/tatelucky/yduts/design/decorator/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * 装饰器模式
+ *
+ * @author tangsheng
+ * @since 2019-06-09
+ */
+package com.tatelucky.yduts.design.decorator;
---
+++ src/main/java/com/tatelucky/yduts/design/ob/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * 观察者模式
+ *
+ * @author tangsheng
+ * @since 2019-06-09
+ */
+package com.tatelucky.yduts.design.ob;
